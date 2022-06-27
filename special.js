620cf8197b6ff04ff5b125009127796fecd347f2
Sidharth S <69863204+sid-the-loser@users.noreply.github.com>
2022-06-27 17:44:55 +0530

    Update special.js

--- special.js
+++ special.js
@@ -5,18 +5,18 @@ var confetti = String.fromCodePoint(0x1F389);
 
 if (dd=="20" && mm=="05"){
 
-    document.write("<h1>"+confetti+"Happy Birthday <i>Keya</i>!"+confetti+"</h1>")
+    document.write("<h1>"+confetti+"Happy Birthday <i>Keya</i>"+confetti+"</h1>")
 
 };
 
 if (dd=="01" && mm=="07"){
 
-    document.write("<h1>"+confetti+"Happy Birthday <i>Sid</i>!"+confetti+"</h1>");
+    document.write("<h1>"+confetti+"Happy Birthday <i>Sid</i>"+confetti+"</h1>");
 
 };
 
 if (dd=="27" && mm=="06"){
 
-    document.write("<h1>"+confetti+"Happy Birthday <i>Aparna</i>!"+confetti+"</h1>");
+    document.write("<h1>"+confetti+"Happy Birthday <i>Aparna</i>"+confetti+"</h1>");
 
 };
