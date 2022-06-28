18bac8804ff3bad0c6f994ca7f7341db34e70f8c
Sidharth S <69863204+sid-the-loser@users.noreply.github.com>
2022-06-28 17:25:23 +0530

    Update special.js

--- special.js
+++ special.js
@@ -3,19 +3,13 @@ var dd = String(today.getDate()).padStart(2, '0');
 var mm = String(today.getMonth() + 1).padStart(2, '0');
 var confetti = String.fromCodePoint(0x1F389);
 
-if (dd=="20" && mm=="05"){
+if (dd=="20" && mm=="05"){ //she is just a friend for me, or used to be my friend!
 
     document.write("<h1>"+confetti+"Happy Birthday <i>Keya</i>"+confetti+"</h1>")
 
 };
 
-if (dd=="01" && mm=="07"){
-
-    document.write("<h1>"+confetti+"Happy Birthday <i>Sid</i>"+confetti+"</h1>");
-
-};
-
-if (dd=="27" && mm=="06"){
+if (dd=="27" && mm=="06"){ // she is my sister
 
     document.write("<h1>"+confetti+"Happy Birthday <i>Aparna</i>"+confetti+"</h1>");
 
