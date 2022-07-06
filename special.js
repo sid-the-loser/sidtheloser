7823ca63c45415fdf3b83431903cc549e9fcfad0
Sidharth S <69863204+sid-the-loser@users.noreply.github.com>
2022-07-06 19:01:37 +0530

    i hope this list iterating format works

--- special.js
+++ special.js
@@ -3,40 +3,20 @@ var dd = today.getDate();
 var mm = today.getMonth() + 1;
 var confetti = String.fromCodePoint(0x1F389);
 const birthdays = [
-
+    // [dd, mm, name]
     [20, 5, "Keya"],
     [27, 6, "Aparna"],
     [1, 7, "Sid"],
-    [6, 7, "Aryan kutta"]
+    [6, 7, "Aryan"]
 
 ];
 
 for(var i = 0, size = birthdays.length; i < size ; i++){
 
-    console.log(birthdays[i])
+    var temp = birthdays[i];
 
-}
-
-if (dd==20 && mm==5){ //she is just a friend for me, or used to be my friend!
-
-    document.write("<h1>"+confetti+"Happy Birthday <i>Keya</i>"+confetti+"</h1>")
-
-};
-
-if (dd==27 && mm==6){ // she is my sister
-
-    document.write("<h1>"+confetti+"Happy Birthday <i>Aparna</i>"+confetti+"</h1>");
-
-};
-
-if (dd==1 && mm==7){ // this is me
-
-    document.write("<h1>"+confetti+"Happy Birthday <i>Sid</i>"+confetti+"</h1>");
-
-};
-
-if (dd==6 && mm==7){ // classmate and friend
-
-    document.write("<h1>"+confetti+"Happy Birthday <i>Aryan</i> kutta"+confetti+"</h1>");
+    if (dd == temp[0] && mm == temp[1]){
+        document.write("<h1>"+confetti+"Happy Birthday <i>"+temp[2]+"</i>"+confetti+"</h1>")
+    };
 
 };
