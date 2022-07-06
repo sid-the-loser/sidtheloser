301589698166d7653d81633606346a84c60735ff
Sidharth S <69863204+sid-the-loser@users.noreply.github.com>
2022-07-06 18:55:03 +0530

    integered all the date and time variables

--- special.js
+++ special.js
@@ -1,6 +1,6 @@
 var today = new Date()
-var dd = String(today.getDate()).padStart(2, '0');
-var mm = String(today.getMonth() + 1).padStart(2, '0');
+var dd = today.getDate();
+var mm = today.getMonth() + 1;
 var confetti = String.fromCodePoint(0x1F389);
 const birthdays = [
 
@@ -13,25 +13,25 @@ const birthdays = [
 
 console.log(dd, mm)
 
-if (dd=="20" && mm=="05"){ //she is just a friend for me, or used to be my friend!
+if (dd==20 && mm==5){ //she is just a friend for me, or used to be my friend!
 
     document.write("<h1>"+confetti+"Happy Birthday <i>Keya</i>"+confetti+"</h1>")
 
 };
 
-if (dd=="27" && mm=="06"){ // she is my sister
+if (dd==27 && mm==6){ // she is my sister
 
     document.write("<h1>"+confetti+"Happy Birthday <i>Aparna</i>"+confetti+"</h1>");
 
 };
 
-if (dd=="01" && mm=="07"){ // this is me
+if (dd==1 && mm==7){ // this is me
 
     document.write("<h1>"+confetti+"Happy Birthday <i>Sid</i>"+confetti+"</h1>");
 
 };
 
-if (dd=="06" && mm=="07"){ // classmate and friend
+if (dd==6 && mm==7){ // classmate and friend
 
     document.write("<h1>"+confetti+"Happy Birthday <i>Aryan</i> kutta"+confetti+"</h1>");
 
