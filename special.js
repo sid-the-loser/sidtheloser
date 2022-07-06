17f551c0d38f8357161eadf9311e6bc79c1e3a71
Sidharth S <69863204+sid-the-loser@users.noreply.github.com>
2022-07-06 18:46:19 +0530

    redid somethings

--- special.js
+++ special.js
@@ -1,5 +1,5 @@
 var today = new Date()
-var dd = (today.getDate()).padStart(2, '0');
+var dd = String(today.getDate()).padStart(2, '0');
 var mm = String(today.getMonth() + 1).padStart(2, '0');
 var confetti = String.fromCodePoint(0x1F389);
 const birthdays = [
@@ -29,7 +29,7 @@ if (dd=="01" && mm=="07"){ // this is me
 
 };
 
-if (dd==6 && mm=="07"){ // classmate and friend
+if (dd=="06" && mm=="07"){ // classmate and friend
 
     document.write("<h1>"+confetti+"Happy Birthday <i>Aryan</i> kutta"+confetti+"</h1>");
 
