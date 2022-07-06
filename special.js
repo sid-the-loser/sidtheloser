a2b276f962fe04b74d33866540ae0d6f328b6da7
Sidharth S <69863204+sid-the-loser@users.noreply.github.com>
2022-07-06 18:49:17 +0530

    aaaaaaaaaaa

--- special.js
+++ special.js
@@ -11,6 +11,8 @@ const birthdays = [
 
 ];
 
+console.log(dd, mm)
+
 if (dd=="20" && mm=="05"){ //she is just a friend for me, or used to be my friend!
 
     document.write("<h1>"+confetti+"Happy Birthday <i>Keya</i>"+confetti+"</h1>")
