a103b22b43e4decd7b99e8f1f8cbe0ee8fbebfb0
Sidharth S <69863204+sid-the-loser@users.noreply.github.com>
2022-07-21 18:47:36 +0530

    big update!!

--- special.js
+++ special.js
@@ -1,4 +1,12 @@
-var today = new Date()
+// website name chooser
+
+const max_titles = 14;
+
+document.write("<img class='title' src='./images/title/"+String(Math.floor(Math.random() * max_titles+1))+".png'>")
+
+// happy brithday greeting
+
+var today = new Date();
 var dd = today.getDate();
 var mm = today.getMonth() + 1;
 var confetti = String.fromCodePoint(0x1F389);
@@ -9,6 +17,7 @@ const birthdays = [
     [1, 7, "Sid"],
     [6, 7, "Aryan"],
     [11, 7, "Jacob"],
+    [9, 7, "Syam"],
 ];
 
 for(var i = 0, size = birthdays.length; i < size ; i++){
@@ -16,7 +25,7 @@ for(var i = 0, size = birthdays.length; i < size ; i++){
     var temp = birthdays[i];
 
     if (dd == temp[0] && mm == temp[1]){
-        document.write("<h1>"+confetti+"Happy Birthday <i>"+temp[2]+"</i>"+confetti+"</h1>")
+        document.write("<h1>"+confetti+"Happy Birthday <i>"+temp[2]+"</i>"+confetti+"</h1>");
     };
 
 };
